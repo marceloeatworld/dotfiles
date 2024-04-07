32bc2697b133a65528b85609bc0e76d2fe771d18
marceloeatworld <20625497+marceloeatworld@users.noreply.github.com>
2024-04-07 16:45:01 +0200

    Update hybrid-luks-btrfs.nix

--- hybrid-luks-btrfs.nix
+++ hybrid-luks-btrfs.nix
@@ -3,7 +3,7 @@
     disk = {
       main = {
         type = "disk";
-        device = "/dev/disk/by-id/ata-Samsung_SSD_850_EVO_250GB_S21PNXAGB12345";
+        device = "/dev/disk/by-id/ata-Samsung_SSD_850_EVO_250GB_S21PNXAGB12345"; # Disque principal
         content = {
           type = "gpt";
           partitions = {
@@ -24,7 +24,7 @@
               size = "100%";
               content = {
                 type = "luks";
-                name = "crypted";
+                name = "crypted-main";
                 settings.allowDiscards = true;
                 # lsblk
                 # passwordFile = "/tmp/secret.key"; # Entrez le mot de passe interactivement
@@ -45,8 +45,37 @@
                       mountOptions = [ "compress=zstd" "noatime" ];
                     };
                     "/swap" = {
-                      mountpoint = "/.swapvol";
-                      swap.swapfile.size = "20M";
+                      mountpoint = "/.swapvol"; 
+                      swap.swapfile.size = "8G";
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+
+      data = {
+        type = "disk";
+        device = "/dev/disk/by-id/ata-WDC_WD10EZEX-00WN4A0_WD-XXXXXXXXXXXX"; # Disque de données
+        content = {
+          type = "gpt";
+          partitions = {
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "crypted-data";
+                settings.allowDiscards = true;
+                # Pas de fichier passwordFile, le mot de passe sera demandé interactivement
+                content = {
+                  type = "btrfs";
+                  extraArgs = [ "-f" ];
+                  subvolumes = {
+                    "/storage" = {
+                      mountpoint = "/storage";
+                      mountOptions = [ "compress=zstd" "noatime" ];
                     };
                   };
                 };
